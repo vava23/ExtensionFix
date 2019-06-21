ca78d54cb452976e26d3f36307f67547e9bca626
Vava <svv15@mail.ru>
2019-06-21 21:40:56 +0400

    Event handling, checks and user message

--- ExtensionFix/ExtensionFixer.cs
+++ ExtensionFix/ExtensionFixer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace ExtensionFix
 {
@@ -10,6 +11,47 @@ namespace ExtensionFix
     public void Main(String aFileName)
     {
 
+    }
+
+    //
+    // Checks if the string contains path to a file with proper extension
+    //
+    public static bool IsValidFile(String aStr)
+    {
+      // Check if its an existing file
+      if(File.Exists(aStr))
+      {
+        // TODO
+        return true;
+      }
+      return false;
+    }
+
+
+    //
+    // Tries to convert file
+    //
+    public static bool ProcessFile(string aFileName)
+    {
+      return true;
+    }
+
+    //
+    // Tries to convert files in the list
+    // Returns number of files converted
+    //
+    public static int ProcessFiles(string[] aFiles)
+    {
+      int cnt = 0;
+      foreach (string tmpFile in aFiles)
+      {
+        if (ExtensionFixer.IsValidFile(tmpFile))
+        {
+          if (ExtensionFixer.ProcessFile(tmpFile))
+          { cnt++; }
+        }
+      }
+      return cnt;
     }
   }
 }
--- ExtensionFix/MainWindow.xaml.cs
+++ ExtensionFix/MainWindow.xaml.cs
@@ -26,5 +26,40 @@ namespace ExtensionFix
       InitializeComponent();
       ExtFixer = new ExtensionFixer();
     }
+
+    //
+    // Determines if the dragged object is a file
+    //
+    private bool FileDropped(DragEventArgs e)
+    {
+      return e.Data.GetDataPresent(DataFormats.FileDrop);
+    }
+
+    private void Border_Drop(object sender, DragEventArgs e)
+    {
+      // Check if a file was dropped
+      if (FileDropped(e))
+      {
+        string userMessage;
+        // Get the list of files
+        string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+        // Process every file
+        int filesFixed = ExtensionFixer.ProcessFiles(files);
+        switch (filesFixed)
+        {
+          case 0:
+            userMessage = "Исправление файлов не требуется";
+            break;
+          case 1:
+            userMessage = "Файл исправлен успешно!";
+            break;
+          default:
+            userMessage = "Файлы исправлены успешно!";
+            break;
+        }
+        // Notify user
+        MessageBox.Show(userMessage);
+      }
+    }
   }
 }
