764e9792d89e70d73f5ebdf6e8f7f62171d1016f
Vava <svv15@mail.ru>
2019-06-28 21:02:02 +0400

    Added an empty test project

---
+++ ExtensionFixTest/ExtensionFixerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionFixTest
+{
+  [TestClass]
+  public class ExtensionFixerTest
+  {
+    [TestMethod]
+    public void TestIsValidFile()
+    {
+      return;
+    }
+    [TestMethod]
+    public void TestProcessFile()
+    {
+      return;
+    }
+    [TestMethod]
+    public void TestProcessFiles()
+    {
+      return;
+    }
+  }
+}
---
+++ ExtensionFixTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExtensionFixTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ExtensionFixTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d4b38b9-e8b7-4062-9aa5-40a00ada103b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
